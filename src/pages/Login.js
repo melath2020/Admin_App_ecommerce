0a87265cbc96f6fcf9181c18f44a322c0dbbcaf4
Aswin s <aswinsudhakaran98@gmail.com>
2023-05-26 11:29:59 +0530

    update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -24,7 +24,7 @@ const Login = () => {
     validationSchema:schema,
     onSubmit: values => {
       dispatch(login(values))
-      window.location.reload()
+      // window.location.reload()
       // alert(JSON.stringify(values, null, 2));
     },
   });
